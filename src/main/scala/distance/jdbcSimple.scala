ad566b234970f41f5a03b2155532e97f0d650af5
ashwin <aashwin@clemson.edu>
2018-04-12 17:10:53 -0400

    organized into multiple fuctions each for specific functionality

--- src/main/scala/distance/jdbcSimple.scala
+++ src/main/scala/distance/jdbcSimple.scala
@@ -16,14 +16,17 @@ import scala.collection.mutable
 import scala.collection.mutable.ListBuffer;
 import scala.math.min
 
+import java.io.{BufferedWriter, FileWriter}
+
+
 object jdbcSimple {
 
   def main(args: Array[String]): Unit = {
 
 
     Class.forName("org.postgresql.Driver")
-    val jdbcUrlOmopDB = "jdbc:postgresql://localhost:5432/omop_postgres"
-    val jdbcUrlOmopCDWDB = "jdbc:postgresql://localhost:5432/omop_postgres"
+    val jdbcUrlOmopDB  = "jdbc:postgresql://localhost:5432/omop_postgres"
+    val jdbcUrlOmopCDWDB = "jdbc:postgresql://localhost:5432/omop_postgres1"
 
 
     //create a spark session
@@ -33,39 +36,47 @@ object jdbcSimple {
       .config("spark.master", "local")
       .getOrCreate()
 
+    collectAndPrintDistance(spark,jdbcUrlOmopDB,jdbcUrlOmopCDWDB)
+  }
+
+  def collectAndPrintDistance(spark: SparkSession , jdbcUrlOmopDB: String, jdbcUrlOmopCDWDB: String): Unit =
+  {
     import spark.implicits._
 
     val connectionProperties = new Properties()
 
+    println(jdbcUrlOmopDB)
+    println(jdbcUrlOmopCDWDB)
     connectionProperties.put("user", "pguser" )
     connectionProperties.put("password", "test")
 
+    //store all the tables in a dataframe
     val dfTableDataOmop = spark.read.format("jdbc").options(Map("url" -> jdbcUrlOmopDB,
-                          "dbtable" -> "(select table_name from information_schema.tables where table_schema ='public') vocab_alias")
-                          ).load()
+      "dbtable" -> "(select table_name from information_schema.tables where table_schema ='public') vocab_alias")
+    ).load()
 
     val dfTableDataOmopCDW = spark.read.format("jdbc").options(Map("url" -> jdbcUrlOmopCDWDB,
-                          "dbtable" -> "(select table_name from information_schema.tables where table_schema ='public') vocab_alias")
-                          ).load()
+      "dbtable" -> "(select table_name from information_schema.tables where table_schema ='public') vocab_alias")
+    ).load()
 
     val tableListOMOP = dfTableDataOmop.select("table_name").map(_.getString(0)).collect.toList
     val tableListOMOPCDW = dfTableDataOmopCDW.select("table_name").map(_.getString(0)).collect.toList
 
-
-    //var resColumnData: java.util.List[String] = null
+    //store all the columns of all tables in a final dataframe for omop database
     var columnListJava: java.util.List[String] = null
     var resColumnDataOMOP = new ListBuffer[String]()
 
     for(i <- 0 until tableListOMOP.length)
-      {
-        var tableName = tableListOMOP(i)
-        val dfColumnDataOMOP = spark.read.format("jdbc").options(Map("url" -> jdbcUrlOmopDB,
-          "dbtable" -> s"(SELECT column_name  FROM information_schema.columns WHERE table_schema = 'public'  AND table_name  = '$tableName') vocab_alias")
-        ).load()
-       val columnList = dfColumnDataOMOP.select("column_name").map(_.getString(0)).collect.toList
-       addToResult(columnList,resColumnDataOMOP)
-      }
+    {
+      var tableName = tableListOMOP(i)
+      val dfColumnDataOMOP = spark.read.format("jdbc").options(Map("url" -> jdbcUrlOmopDB,
+        "dbtable" -> s"(SELECT column_name  FROM information_schema.columns WHERE table_schema = 'public'  AND table_name  = '$tableName') vocab_alias")
+      ).load()
+      val columnList = dfColumnDataOMOP.select("column_name").map(_.getString(0)).collect.toList
+      addToResult(columnList,resColumnDataOMOP)
+    }
 
+    //store all the columns of all tables in a final dataframe for omopcdw database
     var resColumnDataOMOPCDW = new ListBuffer[String]()
 
     for(i <- 0 until tableListOMOPCDW.length)
@@ -98,6 +109,7 @@ object jdbcSimple {
     }
 
   }
+
   //Levenshtein distance metric implemented using dynamic programming instead of recursion technique used in the git repo.
   def distanceBetweenColumns(string1: String, string2: String): Int = {
     val dp = Array.ofDim[Int](string1.length + 1, string2.length + 1)
@@ -122,7 +134,7 @@ object jdbcSimple {
     dp(string1.length)(string2.length)
   }
 
-
+  //function to add the column names to a final list
   def addToResult(strings: scala.List[String], resBuffer: ListBuffer[String]): Unit =
   {
     for(i <- 0 until strings.length)
@@ -131,6 +143,7 @@ object jdbcSimple {
       }
   }
 
+  //function to manipulate omop column values so that there will be a difference in the column names
   def manipulateColValues(strings: scala.List[String], revBuffer: ListBuffer[String]): Unit =
   {
     for(i <- 0 until strings.length)
